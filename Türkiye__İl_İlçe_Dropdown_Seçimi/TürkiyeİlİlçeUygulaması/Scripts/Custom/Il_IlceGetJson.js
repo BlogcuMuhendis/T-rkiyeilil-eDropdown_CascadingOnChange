4150a33db0ac53fdb6c2f266bc71a2230d2296c1
Mert Metin <37337606+mertmtn@users.noreply.github.com>
2019-04-27 21:41:44 +0300

    İlk Commit
    Kodların paylaşılması

---
+++ Türkiye__İl_İlçe_Dropdown_Seçimi/TürkiyeİlİlçeUygulaması/Scripts/Custom/Il_IlceGetJson.js
@@ -0,0 +1,25 @@
+﻿$.getJSON("Models/iller.json", function (sonuc) {
+    $.each(sonuc, function (index, value) {
+        var row = "";
+        row += '<option value="' + value.YerID + '">' + value.YerAdi + '</option>';
+        $("#il").append(row);
+
+    })
+});
+
+
+
+$("#il").on("change", function () {
+    var il = $("#il").val();
+    $("#ilce").attr("disabled", false).html("<option value=''>İlçe Seçiniz...</option>");
+    $.getJSON("Models/ilceler.json", function (sonuc) {
+        $.each(sonuc, function (index, value) {
+            var row = "";
+            if (value.SehirID == il) {
+                row += '<option value="' + value.YerID + '">' + value.YerAdi + '</option>';
+                $("#ilce").append(row);
+            }
+        });
+    });
+});
+
---
+++ Türkiye__İl_İlçe_Dropdown_Seçimi/TürkiyeİlİlçeUygulaması/Scripts/Custom/il_ilceGetirUsingAjax.js
@@ -0,0 +1,36 @@
+﻿$.ajax({
+    url: 'Models/iller.json',
+    dataType: 'json',
+    type: 'GET',
+    data: "",
+    success: function (data) {
+        for (row in data) {
+            $('#il').append($('<option></option>').attr('value', data[row].YerID).text(data[row].YerAdi));
+        }
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+        alert(errorThrown);
+    }
+});
+
+
+
+$("#il").change(function () {
+    $("#ilce").attr("disabled", false).html("<option value=''>İlçe Seçiniz..</option>");
+    $.ajax({
+        url: 'Models/ilceler.json',
+        dataType: 'json',
+        type: 'GET',
+        data: "",
+        success: function (data) {
+            for (row in data) {
+                if (data[row].SehirID == $("#il").val()) {
+                    $('#ilce').append($('<option></option>').attr('value', data[row].YerID).text(data[row].YerAdi));
+                }
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert(errorThrown);
+        }
+    });
+})
